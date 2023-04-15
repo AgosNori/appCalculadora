c326a1e8c2176f9079c05d0258819cf364383bd4
byagos <agosnoriega2018@gmail.com>
2023-04-15 11:39:22 -0300

    agrege todos los archivos

---
+++ multiplicar - copia.js
@@ -0,0 +1,13 @@
+function multiplicar(num1,num2){
+    if (num1 == 0){
+        return 0
+    }
+    if (num2 == 0){
+        return 0
+    }
+    else{
+        return num1*num2
+    }
+   
+}
+module.exports = {multiplicar};
