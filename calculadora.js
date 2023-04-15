11cf3c30a3fc6e1b4f48812e4916123a7cf2bb93
byagos <agosnoriega2018@gmail.com>
2023-04-15 11:06:05 -0300

    agrege el archivo de calculadora

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+const { suma } = require('./suma');
+const {resta} = require('./resta');
+const {multiplicar} = require('./multiplicar')
+const {division} = require('./division')
+
+
+
+console.log(suma(2,2))
+console.log(resta(4,2))
+console.log(multiplicar(2,2))
+console.log(division(4,2))
